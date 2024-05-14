296ed4e79889175c072918b2176e6d7165505b03
xen <egerromaan@gmail.com>
2024-05-14 12:17:44 +0600

    final

--- StringFormatter/src/main/java/org/example/Formatter.java
+++ StringFormatter/src/main/java/org/example/Formatter.java
@@ -1,4 +1,55 @@
 package org.example;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Formatter {
+    public String stringFormatter(String data){
+
+        char[] array = data.toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(array[0]);
+
+        int countTab = 0;
+        for(int i = 1; i < array.length; i++){
+            if(array[i-1] == '{' || array[i-1] == '}' || array[i-1] == ';'){
+                sb.append("\n");
+
+                if(array[i-1] == '{' && array[i] != '}'){
+                    countTab++;
+                } else if(array[i-1] == '}' || (array[i-1] == ';' && array[i] == '}')){
+                    countTab--;
+                }
+                for(int j = 0; j < countTab; j++){
+                    sb.append("\t");
+                }
+            }
+
+            sb.append(array[i]);
+        }
+
+        return sb.toString();
+    }
+
+    public String reader(String path) {
+        StringBuilder sb = new StringBuilder();
+        try(FileInputStream fin=new FileInputStream("notes.txt"))
+        {
+            int i;
+            while((i=fin.read())!=-1){
+                sb.append(i);
+            }
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        return sb.toString();
+    }
+
+    public String reader(){
+        Scanner scanner = new Scanner(System.in);
+        String result = scanner.next();
+        return result;
+    }
 }
--- StringFormatter/src/main/java/org/example/Main.java
+++ StringFormatter/src/main/java/org/example/Main.java
@@ -1,8 +1,14 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.StringJoiner;
+
 public class Main {
     public static void main(String[] args) {
+        Formatter formatter = new Formatter();
 
-        }
+        String result = formatter.stringFormatter(formatter.reader());
+        
+        System.out.println(result);
     }
 }
